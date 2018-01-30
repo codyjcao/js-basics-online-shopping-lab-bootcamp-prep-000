796d69a36f7ee2afe17010ee9984f8ed7f722e72
cjc7@williams.edu Cao <cjc7@williams.edu>
2018-01-30 16:45:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,38 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var thing = {}
+ thing[item] = Math.floor(100*Math.random())
+ cart.push(thing)
+ console.log(`${item} has been added to your cart`)
+ return cart
 }
 
 function viewCart() {
   // write your code here
+  
+  if (cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  }
+  
+  else{
+    var statement = "In your cart, you have "
+    
+    for (var i = 0; i < cart.length; i++){
+      
+      if (i === cart.length-1){
+        statement += `and ${cart[i][0]} at $${cart[i][1]}`
+      }
+      
+      else {
+        statement += `${cart[i][0]} at $${cart[i][1]}`
+      }
+    }
+    console.log(statement)
+  }
 }
+
+
 
 function total() {
   // write your code here
