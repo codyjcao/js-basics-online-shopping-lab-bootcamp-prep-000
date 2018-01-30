d9cbfaddb29b8917b82a1a967c8910909240c0b0
cjc7@williams.edu Cao <cjc7@williams.edu>
2018-01-30 17:48:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,17 +64,14 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  try{
     for (var i = 0; i < cart.length; i++){
       if (Object.keys(cart[i])[0] == item){
         cart.splice(i,1)
         return cart
       }
     }
-    
-  }
-
-  
+  console.log("That item is not in your cart.")
+  return cart
 }
 
 function placeOrder(cardNumber) {
