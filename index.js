878d65523a3195453e19bf1df772f69167a27755
cjc7@williams.edu Cao <cjc7@williams.edu>
2018-01-30 17:50:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,4 +76,11 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber == null){
+    console.log("Sorry, we don't have a credit card on file for you.")
+    return
+  }
+  
+  console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber} `)
+  
 }
