9cb5ddb47d6e38679fbf8aa2a53c8729c8031fe9
cjc7@williams.edu Cao <cjc7@williams.edu>
2018-01-30 17:50:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ function placeOrder(cardNumber) {
     return
   }
   
-  console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber} `)
+  console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
+  
   
 }
